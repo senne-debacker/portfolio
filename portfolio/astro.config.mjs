9d302f1b9731d32731a5ab9c09169f73d7eeb63e
Senne De Backer <senne.de.backer@student.howest.be>
2026-07-18 16:18:13 +0200

    fix: update site URL in astro.config.mjs to correct domain

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro:config';
 
 export default defineConfig({
   // Put your exact custom domain name here
-  site: 'https://debacker.com',
+  site: 'https://github.com/senne-debacker/portfolio.com',
 });
